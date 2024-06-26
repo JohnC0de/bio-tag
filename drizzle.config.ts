cf2f4790a4adb50d18ac38013d7c4a88776b99eb
JohnC0de <joaocarlos.m014@gmail.com>
2024-06-26 15:45:40 -0300

    chore: add turso settings

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,11 @@ import { env } from "@/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
+  driver:'turso',
   dialect: "sqlite",
   dbCredentials: {
     url: env.DATABASE_URL,
+    authToken: env.DATABASE_AUTH_TOKEN,
   },
   tablesFilter: ["bio-tag_*"],
 } satisfies Config;
